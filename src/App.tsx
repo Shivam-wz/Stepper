d8647d91c402a4e0af7340728f24ee9fb700ecb3
Shivam-wz <56469003+Shivam-wz@users.noreply.github.com>
2024-08-07 10:49:26 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LearnerStepper from "./components/LearnerStepper";
+// import ReactHookForm from "./components/2-with-react-hook-form
+
+function App() {
+  return (
+    <div>
+      <LearnerStepper />
+    </div>
+  );
+}
+
+export default App;
